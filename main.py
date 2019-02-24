0e51e791447d2234d237a8492eba827f0109dd73
Ryan <rjr61@gmail.com>
2019-02-24 18:41:30 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, render_template, request 
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return redirect("/static/fukuPage.html", code=302)
+
+@app.route('/login', methods=['GET', 'POST'] )
+def login():
+    if request.method == "GET" :
+        return render_template("login.html")
+    elif request.method == "POST":
+        userName = request.form["userName"]
+        password = request.form["password"]
+        return render_template("loginConfirmation.html", userName=userName, password=password)
+    else: 
+    	return "jello"
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
