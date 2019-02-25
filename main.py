0ef1c9fb5d97ca17897ce8fddbc4d1cd95daa714
Ryan <rjr61@gmail.com>
2019-02-24 23:53:28 -0500

    big chungus

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return redirect("static/fukuPage.html", code=302)
+    return render_template("fukuPage.html")
 
 @app.route('/login', methods=['GET', 'POST'] )
 def login():
     if request.method == "GET" :
         return render_template("login.html")
     elif request.method == "POST":
-        userName = request.form["userName"]
+        userName = "Welcome back "+ request.form["userName"]+ "!"
         password = request.form["password"]
-        return render_template("loginConfirmation.html", userName=userName, password=password)
+        return render_template("fukuPage.html", userName=userName, password=password)
     else: 
     	return "Hello"
-
+@app.route('/order', methods=['GET', 'POST'] )
+def order():
+    if request.method == "GET" :
+        return render_template("orderPage.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
