7e6f55a6bdc01712a822e00f56c039fc7f1fb344
Ryan <rjr61@gmail.com>
2019-02-24 19:14:54 -0500

    display Welcome user

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def login():
     if request.method == "GET" :
         return render_template("login.html")
     elif request.method == "POST":
-        userName = request.form["userName"]
+        userName = "Welcome Back, "+ request.form["userName"] + "!"
         password = request.form["password"]
-        return render_template("loginConfirmation.html", userName=userName, password=password)
+        return redirect("/static/fukuPage.html", userName=userName, password=password)
     else: 
     	return "jello"
 
