5157bb857ab75817f362ed4cca4d5b7adc9a4c8a
Brandon <brandonhe87@gmail.com>
2019-03-05 16:13:46 -0500

    not done yet with functionality yet

--- static/js/customDrink.js
+++ static/js/customDrink.js
@@ -1,6 +1,17 @@
 let drinks = [];
+let toppings = [];
 
-let drink = {
+function Drink(size, tea, flavor, milk, sweetness, toppings, temp) {
+    this.size = size;
+    this.tea = tea;
+    this.flavor = flavor;
+    this.milk = milk;
+    this.sweetness = sweetness;
+    this.toppings = toppings;
+    this.temp = temp;
+}
+
+/*let drink = {
 							size: "",
 							tea: "",
 							flavor: "",
@@ -8,52 +19,45 @@ let drink = {
 							sweetness: "",
 							toppings: [],
 							temperature: "",
-						};
+						};*/
 
-function addSize(size) {
-  drink.size = size;
+function confirmDrink(form) {
+  console.log(form);
+  console.log("I HATE WEB DEV");
+  var sizes = form.elements["size"];
+  for (var i=0; i<sizes.length; i++) {
+    if (sizes[i].checked) {
+      var size = sizes[i].value;
+      break;
+    }
+  }
+
+  var teas = form.elements["tea"];
+  for (var i=0; i<teas.length; i++) {
+    if (teas[i].checked) {
+      var tea = teas[i].value;
+      break;
+    }
+  }
+
+  drink = new Drink(size, tea, flavor, milk, sweetness, toppings, temp);
+  document.write(size, tea);
+  if(drink.size === "" || drink.tea == "" || drink.flavor === ''|| drink.milk === "" || drink.sweetness === "" || drink.temperature === "") {
+    alert("Your Drink Order Form Isn't Complete!");
+  } else if (confirm("Your Drink: ", drink.size, drink.tea)) {
+    drinks.push(drink);
+    alert("Your Drinks: ", drinks);
+  }
 }
 
-function addTea(tea) {
-	drink.tea = tea;
+function addTopping(topping) {
+  toppings.push(topping);
 }
 
-function addFlavor(flavor) {
-	drink.flavor = flavor;
+function displayJelly() {
+
 }
 
-function addMilk(milk) {
-	drink.milk = milk;
-}
+function displayPoppingBoba() {
 
-function addSweetness(sweetness) {
-	drink.sweetness = sweetness;
-}
-
-function addToppings(topping) {
-	drink.toppings.add(topping);
-}
-
-function addTemperature(temp) {
-	drink.temperature = temp;
-}
-
-function confirmDrink() {
-	alert("hello world");
-    drink.size = document.getElementById("size").value;
-    drink.tea = document.getElementById("tea").value;
-  if(drink.size === "" || 
-     drink.tea == "" || 
-		 drink.flavor === ''|| 
-		 drink.milk === "" || 
-		 drink.sweetness === "" || 
-		 drink.temperature === "") {
-			 alert("Your Drink Order Form Isn't Complete!");
-		 } else if (confirm("Your Drink: ", drink.size, drink.tea)) {
-				let table = document.getElementById('cart_table');
-				let drinkSize = table.insertRow(1);
-				drinkSize.innerHTML = drink.size;
-				let drinkTea = table.insertRow(2);
-				drinkTea.innerHTML = drink.tea;
-		}
 }
